e76d10cfaa8d7568cca10b50719627492421fda0
Ng Cheuk Fung <chalkfung@gmail.com>
2022-09-14 19:21:47 +0800

    Skeleton Vue Frontend, to bind api and loading modal

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <h1>
+        URL Shortener Assignment
+      </h1>
+    </v-app-bar>
+
+    <v-main>
+      <Shortener/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Shortener from './components/Shortener';
+
+export default {
+  name: 'App',
+
+  components: {
+    Shortener,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ vue-frontend/src/components/Shortener.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-text-field label="Enter Link to Shorten" v-model="originalLink"></v-text-field>
+        <v-btn elevation="2">Convert</v-btn>
+        <v-text-field :readonly="true" label="Converted Link"></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
